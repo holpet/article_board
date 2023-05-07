16af411505d82868338762597df42136aae4d44f
holpet <45787742+holpet@users.noreply.github.com>
2023-05-07 19:59:24 +0200

    post detail refactoring

--- src/components/posts/post_detail/PostDetail.tsx
+++ src/components/posts/post_detail/PostDetail.tsx
@@ -1,36 +1,21 @@
 import "../Posts.css";
 import "./PostDetail.css";
-import { useCallback } from "react";
-import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { fetchPostDetailData } from "../../../lib/fetchers/fetchUtils";
 import { useQuery } from "@tanstack/react-query";
 import Wrapper from "../../wrapper/Wrapper";
 import Icon from "@mdi/react";
-import {
-  mdiClover,
-  mdiArrowUUpLeftBold,
-  mdiArrowLeftThin,
-  mdiCardsHeart,
-} from "@mdi/js";
+import { mdiArrowUUpLeftBold, mdiArrowLeftThin } from "@mdi/js";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { default as spinner } from "../../../assets/spinner_loader2.svg";
 import Error from "../../error/Error";
+import Article from "./components/Article";
+import Comments from "./components/Comments";
 
 const PostDetail = () => {
   const { postId } = useParams();
   const navigate = useNavigate();
-
-  // (if requested) scroll to comments section
-  const loc = useLocation();
-  const commentsRef = useCallback(
-    (node: HTMLDivElement | null) => {
-      if (node !== null && loc.hash) {
-        node.scrollIntoView({ behavior: "smooth" });
-      } else window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    },
-    [loc]
-  );
 
   // query post detail data
   const postDetailQuery = useQuery({
@@ -56,47 +41,16 @@ const PostDetail = () => {
             </div>
           </div>
           {/* ARTICLE SECTION */}
-          <section className="detail content">
-            <div className="heart">
-              <Icon path={mdiCardsHeart} size={5} />
-            </div>
-            <article>
-              <div className="title">
-                <h1>{postDetailQuery.data.title}</h1>
-              </div>
-              <h3>
-                <span>
-                  <Icon path={mdiClover} size={1} className="clover" />
-                  &nbsp;{postDetailQuery.data.author.name}&nbsp;
-                </span>
-                <span>
-                  @<i>{postDetailQuery.data.author.username}</i>
-                </span>
-              </h3>
-              <p>{postDetailQuery.data.text}</p>
-            </article>
-          </section>
+          <Article
+            title={postDetailQuery.data.title}
+            name={postDetailQuery.data.author.name}
+            username={postDetailQuery.data.author.username}
+            text={postDetailQuery.data.text}
+          />
           <br />
           {/* COMMENT SECTION */}
-          <section className="detail content">
-            <div id="comments" ref={commentsRef}>
-              <div className="comments-bg">
-                <h3>Comments</h3>
-              </div>
-              {postDetailQuery.data.comments.map((comment, i) => (
-                <div key={i} className="comment">
-                  <div className="comment-icon">
-                    <Icon path={mdiClover} size={0.6} className="clover" />
-                  </div>
-                  <div className="comment-body">
-                    <h5>{comment.name}</h5>
-                    <h6>{comment.email}</h6>
-                    <p>{comment.body}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
+          <Comments comments={postDetailQuery.data.comments} />
+
           {/* scroll back up */}
           <div className="up-arrow">
             <button
